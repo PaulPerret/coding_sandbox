f4974992517aa52b6b89bfc5a60a924fda08f41b
Paul Perret <paul@paulperret.com>
2022-02-22 22:19:16 -0600

    Fixed indents

--- vending_machine.js
+++ vending_machine.js
@@ -1,19 +1,19 @@
-'use strict';
+"use strict";
 const prompt = require("prompt-sync")();
 
 // Values for coin inputs
 const coinValues = {
     q: 25,
     d: 10,
-    n: 5
+    n: 5,
 };
 
 // List of products, containing names and costs
 const productList = [
-    {name: "Doritos", cost: 100},
-    {name: "Takis", cost: 125},
-    {name: "Mike & Ikes", cost: 65},
-    {name: "Cheez-its", cost: 90}
+    { name: "Doritos", cost: 100 },
+    { name: "Takis", cost: 125 },
+    { name: "Mike & Ikes", cost: 65 },
+    { name: "Cheez-its", cost: 90 },
 ];
 
 // Gather input to build up total amount of money
@@ -21,14 +21,12 @@ let done = false;
 let total = 0;
 while (!done) {
     let coin = prompt("Enter a coin: Q, N, D, or S to select item: ");
-    if (coin === 's') {
+    if (coin === "s") {
         done = true;
-    }
-    else if (coin in coinValues) {
+    } else if (coin in coinValues) {
         total += coinValues[coin];
         console.log(`You have inserted: ${toMoney(total)}`);
-    }
-    else {
+    } else {
         console.log("I don't know what that is.");
     }
 }
@@ -42,37 +40,37 @@ while (total >= minimum) {
     if (choice.toLowerCase() === "r") {
         console.log(`Coin return chosen`);
         break;
-    }
-    else if (!(Number.isNaN(parseInt(choice)))) {
+    } else if (!Number.isNaN(parseInt(choice))) {
         if (productList.length < Number(choice)) {
             console.log(`Please make a valid selection.`);
-        }
-        else {
-            let product = productList[Number(choice)-1];
+        } else {
+            let product = productList[Number(choice) - 1];
             if (product.cost <= total) {
                 total -= product.cost;
-                console.log(`Enjoy your ${product.name}! You have ${toMoney(total)} left.`)
-            }
-            else {
-                console.log(`You don't have enough money for ${product.name}. Please make another selection.`);
+                console.log(
+                    `Enjoy your ${product.name}! You have ${toMoney(total)} left.`
+                );
+            } else {
+                console.log(
+                    `You don't have enough money for ${product.name}. Please make another selection.`
+                );
             }
         }
     }
- }
+}
 
- console.log(`\nHere's your change: ${toMoney(total)}`);
-
+console.log(`\nHere's your change: ${toMoney(total)}`);
 
 // Format a number of cents into a readable dollar amount
 function toMoney(value) {
-    return "$"+(value/100).toFixed(2);
-};
+    return "$" + (value / 100).toFixed(2);
+}
 
 // Prints the choices to the user from the product list
 function printChoices(products) {
     console.log("\nPlease make your selection:");
     for (let i = 0; i < products.length; i++) {
-        console.log(` ${i+1}. ${products[i].name}: ${toMoney(products[i].cost)}`);
+        console.log(` ${i + 1}. ${products[i].name}: ${toMoney(products[i].cost)}`);
     }
     console.log(` (R. Coin return)`);
 }
