b47d929e4a11c926495c9d2a12b3e92712b28146
Paul Perret <perret318@gmail.com>
2022-06-23 12:12:14 -0500

    Update java-broke.java

--- java-broke.java
+++ java-broke.java
@@ -1,20 +1,6 @@
-package com.idontexist;
 
-import java.util.List;
-import java.util.logging.Logger;
-
-/**
- * @author paul
- */
-@RestController
-public class RespondentController {
-    
-    private static final Logger LOGGER = Logger.getLogger(RespondentController.class.getName());
-
-    @Autowired
-    IRespondentManager respondentManager;
-    
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ResponseEntity<String> testHello() {
-        LOGGER.info("Saying hello");
-        return new ResponseEntity<>("Hello from Gradi
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
